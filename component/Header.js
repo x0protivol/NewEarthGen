c855fa25b411936bc35cfe65895c07c38af01192
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2023-05-19 16:30:52 +0100

    Style header

--- component/Header.js
+++ component/Header.js
@@ -1,8 +1,21 @@
+import Link from "next/link"
+import Image from "next/image"
+import "../Style/header.css"
 export default function Header() {
   return (
-    <>
-      <p>Home</p>
-      <p>Project</p>
-    </>
+    <header className="header">
+      <nav className="nav">
+        <div className="logoFlex">
+          <Image src='/new_earth.png' alt="New Gen Earth Logo" width={200} height={130} />
+          <span>New Gen Earth</span>
+        </div>
+        <div className="justify">
+          <Link href="" className="link">Home</Link>
+        </div>
+        <div>
+          <Link href="" className="link">Project</Link>
+        </div>
+      </nav>
+    </header>
   )
 }
