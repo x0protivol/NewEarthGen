a4edd5e5eedefec2c3b792d130431da8bc0ca722
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2023-05-20 23:49:17 +0100

    Style home page

---
+++ component/Particles.js
@@ -0,0 +1,188 @@
+"use client"; // This is a client component
+
+import { useCallback } from "react";
+import { loadFull } from "tsparticles";
+import Particles from "react-particles";
+//import particlesConfig from "../config/particles-config";
+
+const ParticlesBackground = () => {
+  const particlesInit = useCallback( async (engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback( async (container) => {
+    console.log(container);
+    // container.element.style.position = "absolute";
+    // container.element.style.zIndex = 0;
+  }, []);
+
+  return (
+    <div id="particles-container">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#b5e48c",
+            },
+          },
+          style: {
+            position: "absolute",
+            height: "80%",
+            top: "20%",
+          },
+          // position: "absolute",
+          // zIndex: "0",
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useEffect, useRef } from 'react';
+// import { tsParticles } from 'tsparticles';
+
+// const Particles = () => {
+//   const particlesContainer = useRef(null);
+
+//   useEffect(() => {
+//     tsParticles.load('particles-container', {
+//       /* Your particle configuration options go here */
+//       particles: {
+//         number: {
+//           value: 80, // Number of particles
+//           density: {
+//             enable: true,
+//             value_area: 800, // Area size where particles will be distributed
+//           },
+//         },
+//         color: {
+//           value: '#ffffff', // Particle color
+//         },
+//         shape: {
+//           type: 'circle', // Particle shape (e.g., "circle", "square", "triangle")
+//         },
+//         size: {
+//           value: 5, // Particle size
+//         },
+//         move: {
+//           enable: true,
+//           speed: 2, // Particle movement speed
+//           direction: 'none', // Particle movement direction (e.g., "none", "top", "top-right")
+//           random: false, // Randomize particle movement direction
+//           straight: false, // Make particles move in a straight line
+//           out_mode: 'out', // Particle out mode (e.g., "out", "bounce")
+//         },
+//       },
+//       interactivity: {
+//         events: {
+//           onHover: {
+//             enable: true,
+//             mode: 'repulse', // Interaction mode on hover (e.g., "grab", "repulse")
+//           },
+//         },
+//       },
+//       retina_detect: true, // Enable retina display support
+//     });
+//   }, []);
+
+//   return <div id="particles-container" ref={particlesContainer}></div>;
+// };
+
+export default ParticlesBackground;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,47 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import '../../Style/page.css';
+import ParticlesBackground from '../../component/Particles';
 
 export default function Home() {
   return (
-    <>Hello</>
+    <>
+      <main className='main'>
+        <ParticlesBackground />
+        <div className='text-cover'>
+          <h1 className='main-text'>
+            New Gen <br/> Earth
+          </h1>
+          <p>Making the Earth a cleaner and better place one day at a time</p>
+        </div>
+      </main>
+      <section className='grid-container'>
+          <div className='cover'>
+            <Image src="/rreel.jpg" alt="rrel logo" width={80} height={80} />
+            <h2>RREEL</h2>
+            <p>RREEL is a protocol for the preservation of memories as digital records, minted as non-fungible tokens on the blockchain under the ERC-721 standard and as RREEL token by which theaters can do rewarding to users.</p>
+          </div>
+          <div className='cover'>
+            <Image src="/ResearchClear.png" alt="Research logo" width={80} height={80} />
+            <h2>Research Clear</h2>
+            <p>ResearchClear is an innovative blockchain-powered platform designed to revolutionize the landscape of academic research. With a mission to address the challenges faced by researchers and enhance the efficiency and transparency of the research process, ResearchClear aims to create a decentralized ecosystem that fosters collaboration, rewards quality contributions, and provides a secure and accessible platform for researchers to showcase their work.</p>
+          </div>
+          {/* Beltcoin: just a video */}
+          <div className='cover'>
+            <Image src="/rreel.jpg" alt="rrel logo" width={80} height={80} />
+            <h2>RREEL</h2>
+            <p>RREEL is a protocol for the preservation of memories as digital records, minted as non-fungible tokens on the blockchain under the ERC-721 standard and as RREEL token by which theaters can do rewarding to users.</p>
+          </div>
+          <div className='cover'>
+            <Image src="/equinenft.png" alt="rrel logo" width={80} height={80} />
+            <h2>EquineNFT</h2>
+            <p>EquineNFT aims to bring the world of horse ownership into the digital age. By combining advanced technology with the traditional industry of horse ownership, EquineNFT offers a platform for creating unique NFT IDs for each horse. These NFTs serve as secure and reliable proof of ownership that can be used for buying, selling, and trading horses.</p>
+          </div>
+          <div className='cover align'>
+            <Image src="/DESTINY.png" alt="rrel logo" width={80} height={80} />
+            <h2>Destiny Token Project</h2>
+            <p>EquineNFT aims to bring the world of horse ownership into the digital age. By combining advanced technology with the traditional industry of horse ownership, EquineNFT offers a platform for creating unique NFT IDs for each horse. These NFTs serve as secure and reliable proof of ownership that can be used for buying, selling, and trading horses. The EquineNFT platform also includes a comprehensive marketplace where owners can showcase their horses to potential buyers, upload detailed information and images, and establish a breeding plan.</p>
+          </div>
+      </section>
+    </>
   )
 }
