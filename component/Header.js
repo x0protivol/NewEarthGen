e9a3da2a6554012434056d5a6cbd50c831c58ea4
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2023-05-22 22:49:07 +0100

    Make navbar responsive

--- component/Header.js
+++ component/Header.js
@@ -3,11 +3,19 @@ import Link from "next/link";
 import Image from "next/image";
 // import { useRouter } from "next/navigation";
 import { useState } from 'react';
+// import Font Awesome CSS
+//import "@fortawesome/fontawesome-svg-core/styles.css";
+// Import the FontAwesomeIcon component
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../Style/header.css";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Header() {
   // const router = useRouter();
   const [activeLink, setActiveLink] = useState(null);
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const showNavBar = () => setNavbarOpen(!navbarOpen);
 
   const handleLinkClick = (path) => {
     setActiveLink(path);
@@ -20,10 +28,20 @@ export default function Header() {
           <Image src='/new_earth.png' alt="New Gen Earth Logo" width={200} height={130} />
           {/* <span>New Gen Earth</span> */}
         </div>
-        {/* <div className="justify">
-
-        </div> */}
-        <ul className="header-ul">
+        <div onClick={showNavBar}>
+          <FontAwesomeIcon
+            icon={faBars}
+            id="menu"
+          />
+        </div>
+        <ul className={navbarOpen ? "header-ul active" : "header-ul"}>
+          <div>
+            <FontAwesomeIcon
+              icon={faClose}
+              id="close"
+              onClick={showNavBar}
+            />
+          </div>
           <li>
             {activeLink !== '/' && (
               <Link href="/" onClick={() => handleLinkClick('/')} className="link">
