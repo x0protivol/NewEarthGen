69ca7c468a0f7f1980b2c84c1c1f732da499ad16
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-05 00:41:29 +0530

    Update page.js

--- src/app/beltcoin/page.js
+++ src/app/beltcoin/page.js
@@ -2,25 +2,15 @@
 import ReactPlayer from "react-player";
 
 const BeltCoin = () => {
-  const videoUrl = ;
   return (
     <main className="main">
-    <div>
-      <h2 className="heading-text">Introduction</h2>
-      <p className="p-text">
-        BeltCoin is a revolutionary WEB3 startup with a vision to restore and preserve the Earths ecosystem while fostering bio diversity. The core focus is safeguarding keystone species within aquatic environments. Employing cutting edge hardware and leveraging cryptocurrency. Beltcoin utilises advanced hardware system designed to purify water for the benefit of single cell organisms creating a optimised environments that fosters the growth and conservation of keystone species. This ensures the delicate balanceof ecosystem remains intact
-      </p>
-      <div className="m-bottom">
-        <ReactPlayer
-          url={videoUrl} 
-          controls={true}
-          playing={true} 
-          width='100%'
-          height='100%'
-        />
+      <div>
+        <h2 className="heading-text">Introduction</h2>
+        <p className="p-text">
+          BeltCoin is a revolutionary WEB3 startup with a vision to restore and preserve the Earths ecosystem while fostering bio diversity. The core focus is safeguarding keystone species within aquatic environments. Employing cutting edge hardware and leveraging cryptocurrency. Beltcoin utilises advanced hardware system designed to purify water for the benefit of single cell organisms creating an optimized environment that fosters the growth and conservation of keystone species. This ensures the delicate balance of the ecosystem remains intact.
+        </p>
       </div>
-    </div>
-  </main>
+    </main>
   );
 };
 
