efeb1de75139b4b239a645099ebb4f526e494629
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-05 00:43:02 +0530

    Update page.js

--- src/app/destiny-token/page.js
+++ src/app/destiny-token/page.js
@@ -2,26 +2,17 @@
 import ReactPlayer from "react-player";
 
 const DestinyToken = () => {
-  const videoUrl = ;
   return (
     <main className="main">
-    <div>
-      <h2 className="heading-text">Introduction</h2>
-      <p className="p-text">
-        DestinyToken (DTP) is a stablecoin in the asset creation in DeFi category. DestinyToken addresses the challenges faced in the crypto market such as high volatility, managing multiple cryptocurrencies, and expensive prices which often lead to missed investment opportunities. By creating an indexed token backed by the top nine cryptocurrencies DestinyToken aims to provide a reliable and accessible solution to investors and traders. DestinyToken works on the principle of combining the value and stability of the top nine cryptocurrencies by market capitalisation
-      </p>
-      <div className="m-bottom">
-        <ReactPlayer
-          url={videoUrl} 
-          controls={true}
-          playing={true} 
-          width='100%'
-          height='100%'
-        />
+      <div>
+        <h2 className="heading-text">Introduction</h2>
+        <p className="p-text">
+          DestinyToken (DTP) is a stablecoin in the asset creation in DeFi category. DestinyToken addresses the challenges faced in the crypto market such as high volatility, managing multiple cryptocurrencies, and expensive prices which often lead to missed investment opportunities. By creating an indexed token backed by the top nine cryptocurrencies DestinyToken aims to provide a reliable and accessible solution to investors and traders. DestinyToken works on the principle of combining the value and stability of the top nine cryptocurrencies by market capitalisation.
+        </p>
       </div>
-    </div>
-  </main>
+    </main>
   );
 };
 
 export default DestinyToken;
+
