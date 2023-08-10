ce6b0ee1fc927a05c5fd9b94a711d9b7f8db7264
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-11 01:36:09 +0530

    Update page.js

--- src/app/beltcoin/page.js
+++ src/app/beltcoin/page.js
@@ -1,28 +1,33 @@
 'use client'; 
-import ReactPlayer from "react-player"; 
- 
-const BeltCoin = () => { 
-  const videoUrl = 'https://vimeo.com/829918389?share=copy'; 
-  return ( 
-    <main className="main"> 
-    <div> 
-      <h2 className="heading-text">Introduction</h2> 
-      <p className="p-text"> 
-        BeltCoin is a revolutionary Web3 startup with a vision to restore and preserve the Earths ecosystem while fostering bio diversity. The core focus is safeguarding keystone species within aquatic environments. Employing cutting edge hardware and leveraging cryptocurrency. Beltcoin utilises advanced hardware system designed to purify water for the benefit of single cell organisms creating a optimised environments that fosters the growth and conservation of keystone species. This ensures the delicate balanceof ecosystem remains intact 
-      </p> 
-      <div className="m-bottom"> 
-        <ReactPlayer 
-          url={videoUrl}  
-          controls={true} 
-          playing={true}  
-          width='100%' 
-          height='100%' 
-        /> 
-      </div> 
-    </div> 
-  </main> 
-  ); 
-}; 
- 
+import ReactPlayer from "react-player";
+
+const BeltCoin = () => {
+  const videoUrl = 'https://vimeo.com/829918389?share=copy';
+
+  return (
+    <main className="main">
+      <section>
+        <header>
+          <h2 className="heading-text">Introduction</h2>
+        </header>
+        <p className="p-text">
+          BeltCoin is a revolutionary Web3 startup with a vision to restore and preserve the Earth's ecosystem while fostering biodiversity. The core focus is on safeguarding keystone species within aquatic environments. By employing cutting-edge hardware and leveraging cryptocurrency, BeltCoin utilizes an advanced hardware system designed to purify water for the benefit of single-cell organisms. This creates optimized environments that foster the growth and conservation of keystone species, ensuring the delicate balance of the ecosystem remains intact.
+        </p>
+      </section>
+
+      <section className="m-bottom">
+        <ReactPlayer
+          url={videoUrl}
+          controls={true}
+          playing={false}
+          width='100%'
+          height='100%'
+        />
+      </section>
+    </main>
+  );
+};
+
 export default BeltCoin;
+
 
