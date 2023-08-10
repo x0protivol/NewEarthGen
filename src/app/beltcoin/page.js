f5b3b65bc68a18a7ee66ab634e2fddcc8126b973
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-11 01:01:01 +0530

    Update page.js

--- src/app/beltcoin/page.js
+++ src/app/beltcoin/page.js
@@ -1,17 +1,29 @@
-'use client';
-import ReactPlayer from "react-player";
+'use client'; 
+import ReactPlayer from "react-player"; 
 
-const BeltCoin = () => {
-  return (
-    <main className="main">
-      <div>
-        <h2 className="heading-text">Introduction</h2>
-        <p className="p-text">
-          BeltCoin is a revolutionary WEB3 startup with a vision to restore and preserve the Earths ecosystem while fostering bio diversity. The core focus is safeguarding keystone species within aquatic environments. Employing cutting edge hardware and leveraging cryptocurrency. Beltcoin utilises advanced hardware system designed to purify water for the benefit of single cell organisms creating an optimized environment that fosters the growth and conservation of keystone species. This ensures the delicate balance of the ecosystem remains intact.
-        </p>
-      </div>
-    </main>
-  );
-};
+const BeltCoin = () => { 
+  const videoUrl = 'https://vimeo.com/829918389?share=copy'; 
+
+  return ( 
+    <main className="main"> 
+      <div> 
+        <h2 className="heading-text">Introduction</h2> 
+        <p className="p-text"> 
+          BeltCoin is a revolutionary WEB3 startup with a vision to restore and preserve the Earth's ecosystem while fostering biodiversity. The core focus is safeguarding keystone species within aquatic environments. Employing cutting-edge hardware and leveraging cryptocurrency, Beltcoin utilises an advanced hardware system designed to purify water for the benefit of single-cell organisms, creating optimised environments that foster the growth and conservation of keystone species. This ensures the delicate balance of the ecosystem remains intact.
+        </p> 
+        <div className="m-bottom"> 
+          <ReactPlayer 
+            url={videoUrl}  
+            controls={true} 
+            playing={false}  // Video will not auto-play
+            width='640px'    // Medium width
+            height='360px'   // Corresponding height for the medium width
+          /> 
+        </div> 
+      </div> 
+    </main> 
+  ); 
+}; 
 
 export default BeltCoin;
+
