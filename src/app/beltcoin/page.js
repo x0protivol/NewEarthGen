8e924d0f0edde3f28cb8bc48f548d530ed027319
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-11 02:10:37 +0530

    Update page.js

--- src/app/beltcoin/page.js
+++ src/app/beltcoin/page.js
@@ -10,9 +10,10 @@ const BeltCoin = () => {
         <header>
           <h2 className="heading-text">Introduction</h2>
         </header>
-        <p className="p-text">
-          BeltCoin is a revolutionary Web3 startup with a vision to restore and preserve the Earth's ecosystem while fostering biodiversity. The core focus is on safeguarding keystone species within aquatic environments. By employing cutting-edge hardware and leveraging cryptocurrency, BeltCoin utilizes an advanced hardware system designed to purify water for the benefit of single-cell organisms. This creates optimized environments that foster the growth and conservation of keystone species, ensuring the delicate balance of the ecosystem remains intact.
-        </p>
+       <p className="p-text">
+  BeltCoin is a revolutionary Web3 startup with a vision to restore and preserve the Earth&apos;s ecosystem while fostering biodiversity. The core focus is on safeguarding keystone species within aquatic environments. By employing cutting-edge hardware and leveraging cryptocurrency, BeltCoin utilizes an advanced hardware system designed to purify water for the benefit of single-cell organisms. This creates optimized environments that foster the growth and conservation of keystone species, ensuring the delicate balance of the ecosystem remains intact.
+      </p>
+
       </section>
 
       <section className="m-bottom">
