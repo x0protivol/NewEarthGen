252d9ba11300e88db7fa97cd6de876fbf62a290d
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-12 05:12:11 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -105,7 +105,7 @@ export default function Home() {
             <Image src="/DESTINY.png" alt='Destiny Token Logo' width={80} height={80} />
             <h2>Destiny Token</h2>
             <p>
-              DestinyToken (DTP) is a stablecoin in the asset creation in DeFi category. DestinyToken addresses the challenges faced in the crypto market such as high volatility, managing multiple cryptocurrencies, and expensive prices which often lead to missed investment opportunities. By creating an indexed token backed by the top nine cryptocurrencies DestinyToken aims to provide a reliable and accessible solution to investors and traders. DestinyToken works on the principle of combining the value and stability of the top nine cryptocurrencies by market capitalisation
+              DestinyToken (DTP) is a stablecoin in the asset creation in DeFi category. 
             </p>
           </Link>
         </div>
