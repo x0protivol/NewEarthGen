74a37377360ca837e3c03b5b9dd4898402c3ce57
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-15 21:20:12 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -53,64 +53,51 @@ export default function Home() {
         </div>
         <div className='products-cover' id='products-section'>
           <h3>Products</h3>
-        </div>
-        <div className='grid-container'>
-          <Link href="/rreel" className='cover'>
-            <Image src="/rreel.jpg" alt="rrel logo" width={80} height={80} />
-            <h2>RREEL</h2>
-            <p>RREEL is a protocol for the preservation of memories as digital records, minted as non-fungible tokens on the blockchain under the ERC-721 standard and as RREEL token by which theaters can do rewarding to users.</p>
-          </Link>
-          <Link href="/research-clear" className='cover'>
-            <Image src="/ResearchClear.png" alt="Research logo" width={80} height={80} />
-            <h2>Research Clear</h2>
-            <p>ResearchClear is an innovative blockchain-powered platform designed to revolutionize the landscape of academic research. With a mission to address the challenges faced by researchers and enhance the efficiency and transparency of the research process.</p>
-          </Link>
-          <Link href="/equine-nft" className='cover'>
-            <Image src="/equinenft.png" alt="rrel logo" width={80} height={80} />
-            <h2>EquineNFT</h2>
-            <p>EquineNFT aims to bring the world of horse ownership into the digital age. By combining advanced technology with the traditional industry of horse ownership, EquineNFT offers a platform for creating unique NFT IDs for each horse. These NFTs serve as secure and reliable proof of ownership that can be used for buying, selling, and trading horses.</p>
-          </Link>
-          {/* Beltcoin: just a video and logo */}
-          <Link href="/beltcoin" className='cover align'>
-            <Image src="/beltcoin.png" alt='Belt Coin Logo' width={80} height={80} />
-            <h2>BeltCoin</h2>
-            <p>BeltCoin is a revolutionary WEB3 startup with a vision to restore and preserve the Earths ecosystem while fostering bio diversity. The core focus is safeguarding keystone species within aquatic environments. Employing cutting edge hardware and leveraging cryptocurrency.</p>
-          </Link>
-             <Link href="/vibrateDNA" className='cover'>
-            <Image src="/vibrateDNA1.png" alt="vibrateDNA logo" width={80} height={80} />
-            <h2>vibrateDNA</h2>
-            <p>vibrateDNA introduces an innovative web3 platform, blending advanced tech with the spheres of health and well-being.</p>
-          </Link>
-          <Link href="/FarmCoin" className='cover'>
-            <Image src="/FarmCoin.png" alt="Farm logo" width={80} height={80} />
-            <h2>Farm Coin</h2>
-            <p>Farm coin is a digital currency designed to address challenges in the agricultural and farming sectors.</p>
-          </Link>
-          <Link href="/FreedomGuard" className='cover'>
-            <Image src="/Freedomguard.png" alt="FreedomGuard logo" width={80} height={80} />
-            <h2>FreedomGuard</h2>
-            <p>FreedomGuard provides a fast peer-to-peer anti-trafficking solution, supported by DAO-driven fundraising and assistance for victims, strengthened by web3 and AI-enhanced data protection.</p>
-          </Link>
-            <Link href="/Omnifinery" className='cover'>
-            <Image src="/omnifinery.png" alt="Omnifinery logo" width={80} height={80} />
-            <h2>Omnifinery</h2>
-            <p>Omnifinery pioneers the digital transformation of the fashion sector, surpassing tangible boundaries through web3.</p>
-          </Link>
-             <Link href="/TimeCoin" className='cover'>
-            <Image src="/time3.png" alt="TimeCoin logo" width={80} height={80} />
-            <h2>Time Coin</h2>
-            <p>Time coin is a cryptocurrency based on time, presenting a Universal Basic Income approach.</p>
-          </Link>
-          <Link href="/destiny-token" className='cover'>
-            <Image src="/DESTINY.png" alt='Destiny Token Logo' width={80} height={80} />
-            <h2>Destiny Token</h2>
-            <p>
-              DestinyToken (DTP) is a stablecoin in the asset creation in DeFi category. 
-            </p>
-          </Link>
-        </div>
-      </section>
-      <Footer />
-    </>
-  )
+</div>
+<div className='grid-container'>
+    <Link href="/rreel" className='cover'>
+        <Image src="/rreel.jpg" alt="rrel logo" width={80} height={80} />
+        <h2>RREEL</h2>
+    </Link>
+    <Link href="/research-clear" className='cover'>
+        <Image src="/ResearchClear.png" alt="Research logo" width={80} height={80} />
+        <h2>Research Clear</h2>
+    </Link>
+    <Link href="/equine-nft" className='cover'>
+        <Image src="/equinenft.png" alt="rrel logo" width={80} height={80} />
+        <h2>EquineNFT</h2>
+    </Link>
+    <Link href="/beltcoin" className='cover align'>
+        <Image src="/beltcoin.png" alt='Belt Coin Logo' width={80} height={80} />
+        <h2>BeltCoin</h2>
+    </Link>
+    <Link href="/vibrateDNA" className='cover'>
+        <Image src="/vibrateDNA1.png" alt="vibrateDNA logo" width={80} height={80} />
+        <h2>vibrateDNA</h2>
+    </Link>
+    <Link href="/FarmCoin" className='cover'>
+        <Image src="/FarmCoin.png" alt="Farm logo" width={80} height={80} />
+        <h2>Farm Coin</h2>
+    </Link>
+    <Link href="/FreedomGuard" className='cover'>
+        <Image src="/Freedomguard.png" alt="FreedomGuard logo" width={80} height={80} />
+        <h2>FreedomGuard</h2>
+    </Link>
+    <Link href="/Omnifinery" className='cover'>
+        <Image src="/omnifinery.png" alt="Omnifinery logo" width={80} height={80} />
+        <h2>Omnifinery</h2>
+    </Link>
+    <Link href="/TimeCoin" className='cover'>
+        <Image src="/time3.png" alt="TimeCoin logo" width={80} height={80} />
+        <h2>Time Coin</h2>
+    </Link>
+    <Link href="/destiny-token" className='cover'>
+        <Image src="/DESTINY.png" alt='Destiny Token Logo' width={80} height={80} />
+        <h2>Destiny Token</h2>
+    </Link>
+</div>
+</section>
+<Footer />
+</>
+)
 }
