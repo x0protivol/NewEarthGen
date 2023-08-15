79b967d2a15e4684e33c173d305338dac9f6e0b6
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-15 20:51:25 +0530

    Update page.js

--- src/app/Omnifinery/page.js
+++ src/app/Omnifinery/page.js
@@ -1,10 +1,8 @@
 'use client' 
-import ReactPlayer from "react-player";
+import React from 'react'; // React must be imported to use JSX
 import "../../../Style/rreel.css";
 
 const Omnifinery = () => {
-  const videoUrl = 'https://vimeo.com/853805450?share=copy';
-
   return (
     <main className="main">
       <section>
@@ -14,25 +12,6 @@ const Omnifinery = () => {
         <p className="p-text">
           Omnifinery spearheads the fashion industry digital evolution, transcending physical limitations using web3.
         </p>
-      </section>
-
-      <section>
-        <header>
-          <h2 className="heading-text">Overview</h2>
-        </header>
-        <p className="p-text">
-        Omnifinery is a web3 Phygital platform, merging digital and physical realms with NFTs for clothing, curating a blend of renowned and emerging talents, entirely in house, it offers a unique blend of art, fashion, and collectibles.
-        </p>
-      </section>
-
-      <section className="m-bottom">
-        <ReactPlayer
-          url={videoUrl}
-          controls={true}
-          playing={false}
-          width='100%'
-          height='100%'
-        />
       </section>
     </main>
   );
