c34d2cae4e387d0eaba9c4c9e43df29a8fd07bbe
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-15 21:00:58 +0530

    Update page.js

--- src/app/destiny-token/page.js
+++ src/app/destiny-token/page.js
@@ -1,9 +1,7 @@
 'use client'; 
-import ReactPlayer from "react-player";
+import React from 'react';  // React must be imported to use JSX
 
 const DestinyToken = () => {
-  const videoUrl = 'https://vimeo.com/829961012?share=copy';
-
   return (
     <main className="main">
       <section>
@@ -14,19 +12,10 @@ const DestinyToken = () => {
           DestinyToken (DTP) is a DeFi stablecoin
         </p>
       </section>
-      
-      <section className="m-bottom">
-        <ReactPlayer
-          url={videoUrl}
-          controls={true}
-          playing={false}
-          width='100%'
-          height='100%'
-        />
-      </section>
     </main>
   );
 };
 
 export default DestinyToken;
+
 
