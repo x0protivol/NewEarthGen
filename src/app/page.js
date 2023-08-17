27fa0c62aaf8d7aa32b8fcec8953516ddade0bc2
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-17 23:32:03 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,29 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 import '../../Style/page.css';
 import ParticlesBackground from '../../component/Particles';
 import Footer from '../../component/Footer';
-import FilterableProjects from './FilterableProjects'; // Ensure correct path
+import { NavigationContext } from 'next/navigation'; // <-- New import
+
+const FilterableProjects = dynamic(() => import('./FilterableProjects'), {
+  ssr: false,
+  loading: () => <p>Loading...</p> // Optional loading component
+});
 
 export default function Home() {
-    const router = useRouter();
-    const filter = router.query.filter || 'planned';
-
-    return (
-        <>
-            <main className='home-main'>
-                <ParticlesBackground />
-                <div className='text-cover'>
-                    <h1 className='main-text'>
-                        Real World Blockchain Applications
-                    </h1>
-                </div>
-            </main>
-            <section>
-                <div className='products-cover' id='products-section'>
-                    <h3>Products</h3>
-                    <div>
-                        <button onClick={() => router.push('?filter=planned')}>Projects Planned</button>
-                        <button onClick={() => router.push('?filter=development')}>Projects in Development</button>
-                    </div>
-                </div>
-                <FilterableProjects filter={filter} />
-            </section>
-            <Footer />
-        </>
-    );
+  return (
+    <NavigationContext.Provider value={{ push: (url) => { window.location.href = url; } }}>
+      <main className='home-main'>
+        <ParticlesBackground />
+        <div className='text-cover'>
+          <h1 className='main-text'>
+            Real World Blockchain Applications
+          </h1>
+        </div>
+      </main>
+      <section>
+        <FilterableProjects />
+      </section>
+      <Footer />
+    </NavigationContext.Provider>
+  );
 }
