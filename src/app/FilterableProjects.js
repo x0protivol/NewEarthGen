01ddd2e709b2f9b02c48e89c344d404e62bd0ac5
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-17 23:28:17 +0530

    Update FilterableProjects.js

--- src/app/FilterableProjects.js
+++ src/app/FilterableProjects.js
@@ -1,38 +1,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import '../../Style/page.css';
-import ParticlesBackground from '../../component/Particles';
-import Footer from '../../component/Footer';
-import FilterableProjects from './FilterableProjects'; // Ensure correct path
 
-export default function Home() {
-    const router = useRouter();
-    const filter = router.query.filter || 'planned';
+export default function FilterableProjects({ filter }) {
+    const projects = [
+        { path: "/rreel", alt: "rrel logo", name: "RREEL", status: "planned" },
+        { path: "/research-clear", alt: "Research logo", name: "Research Clear", status: "planned" },
+        { path: "/equine-nft", alt: "rrel logo", name: "EquineNFT", status: "planned" },
+        { path: "/beltcoin", alt: 'Belt Coin Logo', name: "BeltCoin", status: "planned" },
+        { path: "/vibrateDNA", alt: "vibrateDNA logo", name: "vibrateDNA", status: "planned" },
+        { path: "/FarmCoin", alt: "Farm logo", name: "Farm Coin", status: "planned" },
+        { path: "/FreedomGuard", alt: "FreedomGuard logo", name: "FreedomGuard", status: "planned" },
+        { path: "/Omnifinery", alt: "Omnifinery logo", name: "Omnifinery", status: "planned" },
+        { path: "/TimeCoin", alt: "TimeCoin logo", name: "Time Coin", status: "planned" },
+        { path: "/destiny-token", alt: 'Destiny Token Logo', name: "Destiny Token", status: "planned" }
+    ];
+
+    const filteredProjects = projects.filter(project => project.status === filter);
 
     return (
-        <>
-            <main className='home-main'>
-                <ParticlesBackground />
-                <div className='text-cover'>
-                    <h1 className='main-text'>
-                        Real World Blockchain Applications
-                    </h1>
-                </div>
-            </main>
-            <section>
-                <div className='products-cover' id='products-section'>
-                    <h3>Products</h3>
-                    <div>
-                        <button onClick={() => router.push('?filter=planned')}>Projects Planned</button>
-                        <button onClick={() => router.push('?filter=development')}>Projects in Development</button>
-                    </div>
-                </div>
-                <FilterableProjects filter={filter} />
-            </section>
-            <Footer />
-        </>
+        <div className='grid-container'>
+            {filteredProjects.map(project => (
+                <Link key={project.path} href={project.path} className='cover'>
+                    <Image src={`${project.path}.png`} alt={project.alt} width={80} height={80} />
+                    <h2>{project.name}</h2>
+                </Link>
+            ))}
+        </div>
     );
 }
-
-
