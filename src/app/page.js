ec44d7b013210c1f71105123ec5494e229cca387
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-17 22:47:15 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
 import Image from 'next/image';
-import Link from "next/link"
+import Link from "next/link";
+import { useState } from 'react';
 import '../../Style/page.css';
 import ParticlesBackground from '../../component/Particles';
 import Footer from '../../component/Footer';
 
 export default function Home() {
+  const [filter, setFilter] = useState('planned'); // default is 'planned'
+
+  const projects = [
+    { path: "/rreel", alt: "rrel logo", name: "RREEL", status: "planned" },
+    { path: "/research-clear", alt: "Research logo", name: "Research Clear", status: "planned" },
+    { path: "/equine-nft", alt: "rrel logo", name: "EquineNFT", status: "planned" },
+    { path: "/beltcoin", alt: 'Belt Coin Logo', name: "BeltCoin", status: "planned" },
+    { path: "/vibrateDNA", alt: "vibrateDNA logo", name: "vibrateDNA", status: "planned" },
+    { path: "/FarmCoin", alt: "Farm logo", name: "Farm Coin", status: "planned" },
+    { path: "/FreedomGuard", alt: "FreedomGuard logo", name: "FreedomGuard", status: "planned" },
+    { path: "/Omnifinery", alt: "Omnifinery logo", name: "Omnifinery", status: "planned" },
+    { path: "/TimeCoin", alt: "TimeCoin logo", name: "Time Coin", status: "planned" },
+    { path: "/destiny-token", alt: 'Destiny Token Logo', name: "Destiny Token", status: "planned" }
+  ];
+
+  const filteredProjects = projects.filter(project => project.status === filter);
+
   return (
     <>
       <main className='home-main'>
@@ -19,85 +37,47 @@ export default function Home() {
         <div className='slider'>
         {[...Array(4)].map((_, index) => (
           <div className='slide-track' key={index}>
-            <Link href="/research-clear">
-              <Image src="/ResearchClear.png" alt='Research Clear Logo' width={80} height={80} />
-            </Link>
-            
-            <Link href="/rreel">
-              <Image src="/rreel.jpg" alt='RREEL Logo' width={80} height={80} />
-            </Link>
-            <Link href="/beltcoin">
-              <Image src="/beltcoin.png" alt='Belt Coin Logo' width={80} height={80} />
-            </Link>
-            <Link href="/destiny-token">
-              <Image src="/DESTINY.png" alt='Destiny Token Logo' width={80} height={80} />
-            </Link>
-            <Link href="/equine-nft">
-              <Image src="/equinenft.png" alt='EquineNFT Logo' width={80} height={80} />
-            </Link>
-            <Link href="/FarmCoin">
-              <Image src="/FarmCoin.png" alt='FarmCoin Logo' width={80} height={80} />
-            </Link>
-            <Link href="/FreedomGuard">
-              <Image src="/Freedomguard.png" alt='FreedomGuard Logo' width={80} height={80} />
-            </Link>
-            <Link href="/Omnifinery">
-              <Image src="/omnifinery.png" alt='Omnifinery Logo' width={80} height={80} />
-            </Link>
-            <Link href="/TimeCoin">
-              <Image src="/time3.png" alt='TimeCoin Logo' width={80} height={80} />
-            </Link>
-    
+            {filteredProjects.map(project => (
+              <Link key={project.path} href={project.path}>
+                <Image src={`${project.path}.png`} alt={project.alt} width={80} height={80} />
+              </Link>
+            ))}
           </div>
         ))}
         </div>
         <div className='products-cover' id='products-section'>
           <h3>Products</h3>
-</div>
-<div className='grid-container'>
-    <Link href="/rreel" className='cover'>
-        <Image src="/rreel.jpg" alt="rrel logo" width={80} height={80} />
-        <h2>RREEL</h2>
-    </Link>
-    <Link href="/research-clear" className='cover'>
-        <Image src="/ResearchClear.png" alt="Research logo" width={80} height={80} />
-        <h2>Research Clear</h2>
-    </Link>
-    <Link href="/equine-nft" className='cover'>
-        <Image src="/equinenft.png" alt="rrel logo" width={80} height={80} />
-        <h2>EquineNFT</h2>
-    </Link>
-    <Link href="/beltcoin" className='cover align'>
-        <Image src="/beltcoin.png" alt='Belt Coin Logo' width={80} height={80} />
-        <h2>BeltCoin</h2>
-    </Link>
-    <Link href="/vibrateDNA" className='cover'>
-        <Image src="/vibrateDNA1.png" alt="vibrateDNA logo" width={80} height={80} />
-        <h2>vibrateDNA</h2>
-    </Link>
-    <Link href="/FarmCoin" className='cover'>
-        <Image src="/FarmCoin.png" alt="Farm logo" width={80} height={80} />
-        <h2>Farm Coin</h2>
-    </Link>
-    <Link href="/FreedomGuard" className='cover'>
-        <Image src="/Freedomguard.png" alt="FreedomGuard logo" width={80} height={80} />
-        <h2>FreedomGuard</h2>
-    </Link>
-    <Link href="/Omnifinery" className='cover'>
-        <Image src="/omnifinery.png" alt="Omnifinery logo" width={80} height={80} />
-        <h2>Omnifinery</h2>
-    </Link>
-    <Link href="/TimeCoin" className='cover'>
-        <Image src="/time3.png" alt="TimeCoin logo" width={80} height={80} />
-        <h2>Time Coin</h2>
-    </Link>
-    <Link href="/destiny-token" className='cover'>
-        <Image src="/DESTINY.png" alt='Destiny Token Logo' width={80} height={80} />
-        <h2>Destiny Token</h2>
-    </Link>
-</div>
-</section>
-<Footer />
-</>
-)
+          <div>
+            <label>
+              <input
+                type="radio"
+                value="planned"
+                checked={filter === 'planned'}
+                onChange={() => setFilter('planned')}
+              />
+              Projects Planned
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="development"
+                checked={filter === 'development'}
+                onChange={() => setFilter('development')}
+              />
+              Projects in Development
+            </label>
+          </div>
+        </div>
+        <div className='grid-container'>
+          {filteredProjects.map(project => (
+            <Link key={project.path} href={project.path} className='cover'>
+              <Image src={`${project.path}.png`} alt={project.alt} width={80} height={80} />
+              <h2>{project.name}</h2>
+            </Link>
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </>
+  )
 }
