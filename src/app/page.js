7aa53613197319a9729e1283dd181ee34a39aa50
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-17 23:09:04 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,15 @@
-import Image from 'next/image';
-import Link from "next/link";
-import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 import '../../Style/page.css';
 import ParticlesBackground from '../../component/Particles';
 import Footer from '../../component/Footer';
 
+// Dynamically import the component with SSR turned off
+const FilterableProjects = dynamic(() => import('./FilterableProjects'), {
+  ssr: false,
+  loading: () => <p>Loading...</p> // Optional loading component
+});
+
 export default function Home() {
-  const router = useRouter();
-  const filter = router.query.filter || 'planned';
-
-  const projects = [
-    { path: "/rreel", alt: "rrel logo", name: "RREEL", status: "planned" },
-    { path: "/research-clear", alt: "Research logo", name: "Research Clear", status: "planned" },
-    { path: "/equine-nft", alt: "rrel logo", name: "EquineNFT", status: "planned" },
-    { path: "/beltcoin", alt: 'Belt Coin Logo', name: "BeltCoin", status: "planned" },
-    { path: "/vibrateDNA", alt: "vibrateDNA logo", name: "vibrateDNA", status: "planned" },
-    { path: "/FarmCoin", alt: "Farm logo", name: "Farm Coin", status: "planned" },
-    { path: "/FreedomGuard", alt: "FreedomGuard logo", name: "FreedomGuard", status: "planned" },
-    { path: "/Omnifinery", alt: "Omnifinery logo", name: "Omnifinery", status: "planned" },
-    { path: "/TimeCoin", alt: "TimeCoin logo", name: "Time Coin", status: "planned" },
-    { path: "/destiny-token", alt: 'Destiny Token Logo', name: "Destiny Token", status: "planned" }
- ];
-
-  const filteredProjects = projects.filter(project => project.status === filter);
-
-  const handleFilterChange = (newFilter) => {
-    router.push({
-      pathname: router.pathname,
-      query: { ...router.query, filter: newFilter }
-    });
-  };
-
   return (
     <>
       <main className='home-main'>
@@ -43,53 +22,11 @@ export default function Home() {
       </main>
 
       <section>
-        <div className='slider'>
-          {[...Array(4)].map((_, index) => (
-            <div className='slide-track' key={index}>
-              {filteredProjects.map(project => (
-                <Link key={project.path} href={project.path}>
-                  <Image src={`${project.path}.png`} alt={project.alt} width={80} height={80} />
-                </Link>
-              ))}
-            </div>
-          ))}
-        </div>
-
-        <div className='products-cover' id='products-section'>
-          <h3>Products</h3>
-          <div>
-            <label>
-              <input
-                type="radio"
-                value="planned"
-                checked={filter === 'planned'}
-                onChange={() => handleFilterChange('planned')}
-              />
-              Projects Planned
-            </label>
-            <label>
-              <input
-                type="radio"
-                value="development"
-                checked={filter === 'development'}
-                onChange={() => handleFilterChange('development')}
-              />
-              Projects in Development
-            </label>
-          </div>
-        </div>
-
-        <div className='grid-container'>
-          {filteredProjects.map(project => (
-            <Link key={project.path} href={project.path} className='cover'>
-              <Image src={`${project.path}.png`} alt={project.alt} width={80} height={80} />
-              <h2>{project.name}</h2>
-            </Link>
-          ))}
-        </div>
+        <FilterableProjects />
       </section>
 
       <Footer />
     </>
-  )
+  );
 }
+
