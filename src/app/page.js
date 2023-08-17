bd0debb19f3684143b5a9e712a7094f4c39425c1
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-17 23:50:34 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,34 @@ import ParticlesBackground from '../../component/Particles';
 import Footer from '../../component/Footer';
 
 export default function Home() {
+
+  const projects = [
+    { href: "/research-clear", src: "/ResearchClear.png", alt: "Research Clear Logo", name: "Research Clear", status: "planned" },
+    { href: "/rreel", src: "/rreel.jpg", alt: "RREEL Logo", name: "RREEL", status: "planned" },
+    { href: "/beltcoin", src: "/beltcoin.png", alt: "Belt Coin Logo", name: "BeltCoin", status: "planned" },
+    { href: "/destiny-token", src: "/DESTINY.png", alt: "Destiny Token Logo", name: "Destiny Token", status: "planned" },
+    { href: "/equine-nft", src: "/equinenft.png", alt: "EquineNFT Logo", name: "EquineNFT", status: "planned" },
+    { href: "/FarmCoin", src: "/FarmCoin.png", alt: "FarmCoin Logo", name: "Farm Coin", status: "planned" },
+    { href: "/FreedomGuard", src: "/Freedomguard.png", alt: "FreedomGuard Logo", name: "FreedomGuard", status: "planned" },
+    { href: "/Omnifinery", src: "/omnifinery.png", alt: "Omnifinery Logo", name: "Omnifinery", status: "planned" },
+    { href: "/TimeCoin", src: "/time3.png", alt: "TimeCoin Logo", name: "Time Coin", status: "planned" },
+    // ... add other projects here with their respective status
+  ];
+
+  function filterProjects(status) {
+    const allProjects = document.querySelectorAll('.cover');
+    allProjects.forEach(project => {
+      if (status === 'all' || project.dataset.status === status) {
+        project.style.display = 'block';
+      } else {
+        project.style.display = 'none';
+      }
+    });
+  }
+
+  // Call the filter function immediately after defining it to set the default state
+  filterProjects('planned');
+
   return (
     <>
       <main className='home-main'>
@@ -16,89 +44,24 @@ export default function Home() {
         </div>
       </main>
       <section>
-        <div className='slider'>
-        {[...Array(4)].map((_, index) => (
-          <div className='slide-track' key={index}>
-            <Link href="/research-clear">
-              <Image src="/ResearchClear.png" alt='Research Clear Logo' width={80} height={80} />
-            </Link>
-            
-            <Link href="/rreel">
-              <Image src="/rreel.jpg" alt='RREEL Logo' width={80} height={80} />
-            </Link>
-            <Link href="/beltcoin">
-              <Image src="/beltcoin.png" alt='Belt Coin Logo' width={80} height={80} />
-            </Link>
-            <Link href="/destiny-token">
-              <Image src="/DESTINY.png" alt='Destiny Token Logo' width={80} height={80} />
-            </Link>
-            <Link href="/equine-nft">
-              <Image src="/equinenft.png" alt='EquineNFT Logo' width={80} height={80} />
-            </Link>
-            <Link href="/FarmCoin">
-              <Image src="/FarmCoin.png" alt='FarmCoin Logo' width={80} height={80} />
-            </Link>
-            <Link href="/FreedomGuard">
-              <Image src="/Freedomguard.png" alt='FreedomGuard Logo' width={80} height={80} />
-            </Link>
-            <Link href="/Omnifinery">
-              <Image src="/omnifinery.png" alt='Omnifinery Logo' width={80} height={80} />
-            </Link>
-            <Link href="/TimeCoin">
-              <Image src="/time3.png" alt='TimeCoin Logo' width={80} height={80} />
-            </Link>
-    
-          </div>
-        ))}
+        <div className="filter">
+          <button onClick={() => filterProjects('all')}>All</button>
+          <button onClick={() => filterProjects('planned')} style={{ fontWeight: 'bold' }}>Planned</button>
+          <button onClick={() => filterProjects('in development')}>In Development</button>
+          <button onClick={() => filterProjects('completed')}>Completed</button>
         </div>
-        <div className='products-cover' id='products-section'>
-          <h3>Products</h3>
-</div>
-<div className='grid-container'>
-    <Link href="/rreel" className='cover'>
-        <Image src="/rreel.jpg" alt="rrel logo" width={80} height={80} />
-        <h2>RREEL</h2>
-    </Link>
-    <Link href="/research-clear" className='cover'>
-        <Image src="/ResearchClear.png" alt="Research logo" width={80} height={80} />
-        <h2>Research Clear</h2>
-    </Link>
-    <Link href="/equine-nft" className='cover'>
-        <Image src="/equinenft.png" alt="rrel logo" width={80} height={80} />
-        <h2>EquineNFT</h2>
-    </Link>
-    <Link href="/beltcoin" className='cover align'>
-        <Image src="/beltcoin.png" alt='Belt Coin Logo' width={80} height={80} />
-        <h2>BeltCoin</h2>
-    </Link>
-    <Link href="/vibrateDNA" className='cover'>
-        <Image src="/vibrateDNA1.png" alt="vibrateDNA logo" width={80} height={80} />
-        <h2>vibrateDNA</h2>
-    </Link>
-    <Link href="/FarmCoin" className='cover'>
-        <Image src="/FarmCoin.png" alt="Farm logo" width={80} height={80} />
-        <h2>Farm Coin</h2>
-    </Link>
-    <Link href="/FreedomGuard" className='cover'>
-        <Image src="/Freedomguard.png" alt="FreedomGuard logo" width={80} height={80} />
-        <h2>FreedomGuard</h2>
-    </Link>
-    <Link href="/Omnifinery" className='cover'>
-        <Image src="/omnifinery.png" alt="Omnifinery logo" width={80} height={80} />
-        <h2>Omnifinery</h2>
-    </Link>
-    <Link href="/TimeCoin" className='cover'>
-        <Image src="/time3.png" alt="TimeCoin logo" width={80} height={80} />
-        <h2>Time Coin</h2>
-    </Link>
-    <Link href="/destiny-token" className='cover'>
-        <Image src="/DESTINY.png" alt='Destiny Token Logo' width={80} height={80} />
-        <h2>Destiny Token</h2>
-    </Link>
-</div>
-</section>
-<Footer />
-</>
-)
+        <div className='grid-container'>
+          {projects.map(project => (
+            <Link href={project.href} className='cover' key={project.href} data-status={project.status}>
+              <Image src={project.src} alt={project.alt} width={80} height={80} />
+              <h2>{project.name}</h2>
+            </Link>
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </>
+  )
 }
+
     
