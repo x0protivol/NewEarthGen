c3d4f13712a32ebd035f8f7b3d40ce0b12b80254
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-18 19:04:42 +0530

    Update page.js

--- src/app/TimeCoin/page.js
+++ src/app/TimeCoin/page.js
@@ -1,20 +1,41 @@
-'use client' 
-import React from 'react'; // React must be imported to use JSX
-import "../../../Style/rreel.css";
-
-const TimeCoin = () => {
-  return (
-    <main className="main">
-      <section>
-        <header>
-          <h2 className="heading-text">Introduction</h2>
-        </header>
-        <p className="p-text">
-          Time coin is a cryptocurrency that redefines the concept of value.
-        </p>
-      </section>
-    </main>
-  );
-};
-
+'use client'  
+import ReactPlayer from "react-player"; 
+import "../../../Style/rreel.css"; 
+ 
+const TimeCoin = () => { 
+  const videoUrl = 'https://vimeo.com/853803268?share=copy'; 
+ 
+  return ( 
+    <main className="main"> 
+      <section> 
+        <header> 
+          <h2 className="heading-text">Introduction</h2> 
+        </header> 
+        <p className="p-text"> 
+          Time coin is a cryptocurrency that redefines the concept of value. 
+        </p> 
+      </section> 
+ 
+      <section> 
+        <header> 
+          <h2 className="heading-text">Overview</h2> 
+        </header> 
+        <p className="p-text"> 
+        Time coin is cryptocurrency on time, introducing a Universal Basic Income model and rewarding staking, its time economy system divides each day into 24 units, allowing users to stake tokens hourly and earn rewards. 
+        </p> 
+      </section> 
+ 
+      <section className="m-bottom"> 
+        <ReactPlayer 
+          url={videoUrl} 
+          controls={true} 
+          playing={false} 
+          width='100%' 
+          height='100%' 
+        /> 
+      </section> 
+    </main> 
+  ); 
+}; 
+ 
 export default TimeCoin;
