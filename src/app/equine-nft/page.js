542302102d34d05989fb3f4173e4b8c396ba258c
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-26 23:59:53 +0530

    Update page.js

--- src/app/equine-nft/page.js
+++ src/app/equine-nft/page.js
@@ -1,5 +1,7 @@
-'use client'; 
+'use client'  
 import ReactPlayer from "react-player"; 
+import "../../../Style/rreel.css"; 
+ 
 const EquineNft = () => { 
   const videoUrl = 'https://vimeo.com/858177846?share=copy'; 
  
@@ -10,16 +12,16 @@ const EquineNft = () => {
           <h2 className="heading-text">Introduction</h2> 
         </header> 
         <p className="p-text"> 
-          EquineNFT aims to bring the world of horse ownership into the digital age. By combining advanced technology with the traditional industry of horse ownership, EquineNFT offers a platform for creating unique NFT IDs for each horse. These NFTs serve as secure and reliable proof of ownership that can be used for buying, selling, and trading horses. The EquineNFT platform also includes a comprehensive marketplace where owners can showcase their horses to potential buyers, upload detailed information and images, and establish a breeding plan. 
+          EquineNFT aims to bring the world of horse ownership into the digital age. By combining advanced technology with the traditional industry of horse ownership, EquineNFT offers a platform for creating unique NFT IDs for each horse. These NFTs serve as secure and reliable proof of ownership that can be used for buying, selling, and trading horses. 
         </p> 
       </section> 
-       
+ 
       <section> 
         <header> 
           <h2 className="heading-text">Overview</h2> 
         </header> 
         <p className="p-text"> 
-          EquineNFTs incorporate microchip & QR code technology, enabling horse owners to easily create and manage their horse ownership NFTs on-chain. This technology provides a secure and reliable way of establishing ownership and ensuring the safety of horses. It offers a roadmap for a comprehensive breeding platform that includes staking, betting, and trading marketplaces. With a focus on innovation, security, and user experience, offering a one-stop shop for all things related to horse ownership, breeding, and trading along with staking of NFTs for creating an additional revenue stream for horse owners. 
+         EquineNFTs incorporate microchip & QR code technology, enabling horse owners to easily create and manage their horse ownership NFTs on-chain. This technology provides a secure and reliable way of establishing ownership and ensuring the safety of horses. It offers a roadmap for a comprehensive breeding platform that includes staking, betting, and trading marketplaces. With a focus on innovation, security, and user experience, offering a one-stop shop for all things related to horse ownership, breeding, and trading along with staking of NFTs for creating an additional revenue stream for horse owners. 
           EquineNFT&apos;s platform enables horse owners to upload detailed information and images of their horses, which can be easily accessed and viewed by potential buyers. By allowing horse owners to showcase their horses in a visually appealing and informative way. It helps them to increase the chances of finding the right buyers. Horse owners can add a range of details about their horses, such as age, breed, gender, height, color, and temperament, along with multiple images. 
         </p> 
       </section> 
