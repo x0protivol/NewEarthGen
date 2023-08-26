c42ab544c2fd50d447da874fb3399be258a82215
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-27 00:04:55 +0530

    Update page.js

--- src/app/beltcoin/page.js
+++ src/app/beltcoin/page.js
@@ -1,6 +1,6 @@
-'use client';  
-import ReactPlayer from "react-player";
-import "../../../Style/rreel.css";  
+'use client'  
+import ReactPlayer from "react-player"; 
+import "../../../Style/rreel.css"; 
  
 const BeltCoin = () => { 
   const videoUrl = 'https://vimeo.com/858184646?share=copy'; 
@@ -11,10 +11,18 @@ const BeltCoin = () => {
         <header> 
           <h2 className="heading-text">Introduction</h2> 
         </header> 
-       <p className="p-text"> 
-  BeltCoin is a revolutionary Web3 startup with a vision to restore and preserve the Earth&apos;s ecosystem while fostering biodiversity. The core focus is on safeguarding keystone species within aquatic environments. By employing cutting-edge hardware and leveraging cryptocurrency, BeltCoin utilizes an advanced hardware system designed to purify water for the benefit of single-cell organisms. This creates optimized environments that foster the growth and conservation of keystone species, ensuring the delicate balance of the ecosystem remains intact. 
+        <p className="p-text"> 
+          BeltCoin is a revolutionary Web3 startup with a vision to restore and preserve the Earth&apos;s ecosystem while fostering biodiversity. The core focus is on safeguarding keystone species within aquatic environments. By employing cutting-edge hardware and leveraging cryptocurrency.
       </p> 
+      </section> 
  
+      <section> 
+        <header> 
+          <h2 className="heading-text">Overview</h2> 
+        </header> 
+        <p className="p-text"> 
+        BeltCoin utilizes an advanced hardware system designed to purify water for the benefit of single-cell organisms. This creates optimized environments that foster the growth and conservation of keystone species, ensuring the delicate balance of the ecosystem remains intact. 
+        </p> 
       </section> 
  
       <section className="m-bottom"> 
@@ -31,6 +39,3 @@ const BeltCoin = () => {
 }; 
  
 export default BeltCoin;
-
-
-
