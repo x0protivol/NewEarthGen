d4078bbf8a0d01e7eec89d130197548c77e31d52
x0protivol <94541118+x0protivol@users.noreply.github.com>
2025-06-15 18:13:24 +0530

    Updated next.config.js

--- component/Particles.js
+++ component/Particles.js
@@ -1,21 +1,16 @@
-"use client"; // This is a client component
+"use client";
 
 import { useCallback } from "react";
-import { loadFull } from "tsparticles";
 import Particles from "react-particles";
-//import particlesConfig from "../config/particles-config";
+import { loadSlim } from "tsparticles-slim";
 
 const ParticlesBackground = () => {
-  const particlesInit = useCallback( async (engine) => {
+  const particlesInit = useCallback(async (engine) => {
     console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
+    await loadSlim(engine);
   }, []);
 
-  const particlesLoaded = useCallback( async (container) => {
+  const particlesLoaded = useCallback(async (container) => {
     console.log(container);
   }, []);
 
