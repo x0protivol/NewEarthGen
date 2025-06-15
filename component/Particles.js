e9329d63bd1cf9465b8016baee30cd62b9a35bcf
x0protivol <94541118+x0protivol@users.noreply.github.com>
2025-06-15 18:19:16 +0530

    Make application responsive for desktop, iPhone and Android

--- component/Particles.js
+++ component/Particles.js
@@ -28,8 +28,10 @@ const ParticlesBackground = () => {
           },
           style: {
             position: "absolute",
-            height: "80%",
-            top: "20%",
+            height: "100%",
+            width: "100%",
+            top: "0",
+            left: "0",
           },
           fpsLimit: 120,
           interactivity: {
@@ -46,23 +48,23 @@ const ParticlesBackground = () => {
             },
             modes: {
               push: {
-                quantity: 4,
+                quantity: 2,
               },
               repulse: {
-                distance: 200,
+                distance: 100,
                 duration: 0.4,
               },
             },
           },
           particles: {
             color: {
-              value: "#000",
+              value: "#132a13",
             },
             links: {
-              color: "#000",
-              distance: 150,
+              color: "#132a13",
+              distance: 120,
               enable: true,
-              opacity: 0.5,
+              opacity: 0.3,
               width: 1,
             },
             collisions: {
@@ -75,27 +77,77 @@ const ParticlesBackground = () => {
                 default: "bounce",
               },
               random: false,
-              speed: 3,
+              speed: 1.5,
               straight: false,
             },
             number: {
               density: {
                 enable: true,
-                area: 800,
+                area: 1000,
               },
-              value: 80,
+              value: 50,
             },
             opacity: {
-              value: 0.5,
+              value: 0.4,
             },
             shape: {
               type: "circle",
             },
             size: {
-              value: { min: 1, max: 5 },
+              value: { min: 1, max: 3 },
             },
           },
           detectRetina: true,
+          responsive: [
+            {
+              maxWidth: 768,
+              options: {
+                particles: {
+                  number: {
+                    value: 30,
+                  },
+                  links: {
+                    distance: 100,
+                    opacity: 0.2,
+                  },
+                  move: {
+                    speed: 1,
+                  },
+                },
+                interactivity: {
+                  modes: {
+                    repulse: {
+                      distance: 80,
+                    },
+                  },
+                },
+              },
+            },
+            {
+              maxWidth: 480,
+              options: {
+                particles: {
+                  number: {
+                    value: 20,
+                  },
+                  links: {
+                    distance: 80,
+                    opacity: 0.15,
+                  },
+                  move: {
+                    speed: 0.8,
+                  },
+                },
+                interactivity: {
+                  modes: {
+                    repulse: {
+                      distance: 60,
+                    },
+                  },
+                },
+              },
+            },
+          ],
         }}
       />
     </div>
